1a15829e76e9bde2111526b0ad2a0f7d2820fe5f
Tomas Valle Duran <tvalleduran@mail.austral.edu.com>
2025-05-17 16:33:47 -0300

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS for all routes. Change for whitelist if needed.
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
