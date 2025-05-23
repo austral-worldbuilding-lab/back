e92b51763899c3e21e90578431f9f0fc59bcd2d8
ccatalini <camila.catalini@ing.austral.edu.ar>
2025-05-23 17:59:46 -0300

    swagger disabled in prod

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,22 @@ async function bootstrap() {
   app.enableCors(); // Enable CORS for all routes. Change for whitelist if needed.
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  const config = new DocumentBuilder()
-    .setTitle('AWBL API')
-    .setDescription('Documentación de la API del backend del AWBL')
-    .addBearerAuth()
-    .build();
+  // Only enable Swagger in development and staging environments
+  const environment = process.env.NODE_ENV || 'development';
+  if (environment !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('AWBL API')
+      .setDescription('Documentación de la API del backend del AWBL')
+      .addBearerAuth()
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  });
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document, {
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+    });
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
