60e52d223456fd541e1d421f46bb8779fd3453b4
ccatalini <camila.catalini@ing.austral.edu.ar>
2025-08-23 13:08:14 -0300

    copilot changes

--- src/modules/mandala/types/mandala-type.enum.ts
+++ src/modules/mandala/types/mandala-type.enum.ts
@@ -1,11 +1,3 @@
-/**
- * Enum to define mandala types in the worldbuilding system
- * 
- * - CHARACTER: A mandala that represents a specific character and is linked 
- *              to a parent mandala (i.e., it's a character within another mandala)
- * - UNIFIED: An independent mandala that can contain characters but is not 
- *            itself a character of another mandala
- */
 export enum MandalaType {
   CHARACTER = 'character',
   UNIFIED = 'unified',
