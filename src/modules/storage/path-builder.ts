52a7b9f746acf5d2ea71fbde0b085c6573cf8ca9
Tomas Valle Duran <tvalleduran@mail.austral.edu.com>
2025-08-24 23:03:12 -0300

    improved error messages

--- src/modules/storage/path-builder.ts
+++ src/modules/storage/path-builder.ts
@@ -3,14 +3,16 @@ import { FileScope } from '@modules/files/types/file-scope.type';
 export function buildPrefix(scope: FileScope): string {
   if (scope.mandalaId) {
     if (!scope.projectId || !scope.orgId) {
-      throw new Error('Mandala scope requires projectId and orgId');
+      throw new Error(
+        'Mandala scope requires both projectId and orgId to be defined',
+      );
     }
     return `org/${scope.orgId}/project/${scope.projectId}/mandala/${scope.mandalaId}/files/`;
   }
 
   if (scope.projectId) {
     if (!scope.orgId) {
-      throw new Error('Project scope requires orgId');
+      throw new Error('Project scope requires orgId to be defined');
     }
     return `org/${scope.orgId}/project/${scope.projectId}/files/`;
   }
