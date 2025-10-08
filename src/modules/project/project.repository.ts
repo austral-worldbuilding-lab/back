4841722f9b4e1ea7aadc5452cd14eba940e87243
ccatalini <camila.catalini@ing.austral.edu.ar>
2025-10-08 14:44:57 -0300

    implement simple root projects filter

--- src/modules/project/project.repository.ts
+++ src/modules/project/project.repository.ts
@@ -1,18 +1,8 @@
 import { DimensionDto } from '@common/dto/dimension.dto';
 import { ResourceNotFoundException } from '@common/exceptions/custom-exceptions';
 import { PrismaService } from '@modules/prisma/prisma.service';
-import { Injectable, BadRequestException } from '@nestjs/common';
-import { Prisma, Project, Tag, ProjProvLinkRole } from '@prisma/client';
-
-type ProvocationWithProjects = Prisma.ProvocationGetPayload<{
-  include: {
-    projects: {
-      include: {
-        project: true;
-      };
-    };
-  };
-}>;
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Prisma, Project, ProjProvLinkRole, Tag } from '@prisma/client';
 
 import { CreateProjectFromProvocationDto } from './dto/create-project-from-provocation.dto';
 import { CreateProjectFromQuestionDto } from './dto/create-project-from-question.dto';
@@ -27,7 +17,17 @@ import { UpdateProjectDto } from './dto/update-project.dto';
 import { UserRoleResponseDto } from './dto/user-role-response.dto';
 import { DEFAULT_DIMENSIONS, DEFAULT_SCALES } from './resources/default-values';
 import { ProjectConfiguration } from './types/project-configuration.type';
-import { TimelineGraph, ProjectNode, Edge } from './types/timeline.type';
+import { Edge, ProjectNode, TimelineGraph } from './types/timeline.type';
+
+type ProvocationWithProjects = Prisma.ProvocationGetPayload<{
+  include: {
+    projects: {
+      include: {
+        project: true;
+      };
+    };
+  };
+}>;
 
 @Injectable()
 export class ProjectRepository {
@@ -91,7 +91,7 @@ export class ProjectRepository {
       this.parseToProjectDto(project.project),
     );
 
-    const provocationDto = {
+    return {
       id: provocation.id,
       question: provocation.question,
       title: content?.title,
@@ -101,7 +101,6 @@ export class ProjectRepository {
       updatedAt: provocation.updatedAt,
       projectsOrigin: parsedOriginProjects,
     };
-    return provocationDto;
   }
 
   // find wich project generated this provocation
@@ -500,8 +499,9 @@ export class ProjectRepository {
     skip: number,
     take: number,
     userId: string,
+    rootOnly: boolean = false,
   ): Promise<[ProjectDto[], number]> {
-    const whereClause = {
+    const whereClause: Prisma.ProjectWhereInput = {
       isActive: true,
       userRoles: {
         some: {
@@ -509,6 +509,13 @@ export class ProjectRepository {
         },
       },
     };
+
+    if (rootOnly) {
+      whereClause.rootProjectId = {
+        not: null,
+      };
+    }
+
     const [projects, total] = await this.prisma.$transaction([
       this.prisma.project.findMany({
         where: whereClause,
@@ -521,7 +528,14 @@ export class ProjectRepository {
       }),
     ]);
 
-    return [projects.map((project) => this.parseToProjectDto(project)), total];
+    const filteredProjects = rootOnly
+      ? projects.filter((project) => project.rootProjectId === project.id)
+      : projects;
+
+    return [
+      filteredProjects.map((project) => this.parseToProjectDto(project)),
+      total,
+    ];
   }
 
   async findOne(id: string): Promise<ProjectDto | null> {
