cd52afa4b21925ea70332075feeefd35a27d3660
Tomas Valle Duran <tvalleduran@mail.austral.edu.com>
2025-10-15 13:21:26 +0200

    Eslint fixes

--- src/common/utils/color.utils.ts
+++ src/common/utils/color.utils.ts
@@ -2,4 +2,3 @@ export function generateRandomColor(): string {
   const randomColor = Math.floor(Math.random() * 16777215);
   return '#' + randomColor.toString(16).padStart(6, '0').toUpperCase();
 }
-
--- src/modules/mandala/types/images.ts
+++ src/modules/mandala/types/images.ts
@@ -1,5 +1,5 @@
-import { CreateTagDto } from '@modules/project/dto/create-tag.dto';
 import { Tag } from '@modules/mandala/types/postits';
+import { CreateTagDto } from '@modules/project/dto/create-tag.dto';
 
 export interface MandalaImage {
   id: string;
