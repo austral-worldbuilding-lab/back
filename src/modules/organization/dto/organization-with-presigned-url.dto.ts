9151d21169709090536040bc11fc51a6bcaad585
Tomas Valle Duran <tvalleduran@mail.austral.edu.com>
2025-11-05 21:14:02 +0100

    added banner to org schema and responses of creation and update. Also added extra field in confirmation endpoint to specify which picture is

--- src/modules/organization/dto/organization-with-presigned-url.dto.ts
+++ src/modules/organization/dto/organization-with-presigned-url.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 import { OrganizationDto } from './organization.dto';
 
-export class OrganizationWithPresignedUrlDto extends OrganizationDto {
+class PresignedImageDto {
   @ApiProperty({
     description: 'ID de la imagen a subir (sin extensión)',
     example: 'a1b2c3d4-e5f6-7890-abcd-ef1234567890',
@@ -15,4 +15,18 @@ export class OrganizationWithPresignedUrlDto extends OrganizationDto {
       'https://account.blob.core.windows.net/container/org/123/images/a1b2c3d4-e5f6-7890-abcd-ef1234567890?sig=...',
   })
   presignedUrl!: string;
+}
+
+export class OrganizationWithPresignedUrlDto extends OrganizationDto {
+  @ApiProperty({
+    description: 'Información para subir la foto de perfil',
+    type: PresignedImageDto,
+  })
+  profilePicture!: PresignedImageDto;
+
+  @ApiProperty({
+    description: 'Información para subir la foto de banner',
+    type: PresignedImageDto,
+  })
+  bannerPicture!: PresignedImageDto;
 }
